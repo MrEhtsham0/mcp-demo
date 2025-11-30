1e983a87467f87d4f8d580c8f9e31a2d51a1b264
MrEhtsham0 <mrehtsham0@gmail.com>
2025-11-30 09:58:40 +0500

    updated code for docker

--- app/routes/upload_router.py
+++ app/routes/upload_router.py
@@ -12,6 +12,7 @@ async def upload_file(file: UploadFile = File(...)):
         file_extension = file.filename.split(".")[-1]
         s3_file_name = f"{uuid.uuid4()}.{file_extension}"
         
+        
         # 2. Upload to S3
         s3_object_details = await upload_file_to_s3(
             file_obj=file.file, 
